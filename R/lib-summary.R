cffafcf7b1bbd5ee606b1f4095df5b3e9bf29470
Jenny Bryan <jenny.f.bryan@gmail.com>
2024-06-14 12:05:53 +0200

    Add `sizes` argument; take dependency on fs

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -3,16 +3,28 @@
 #' Provides a brief summary of the package libraries on your machine. This is
 #' Jenny!
 #'
+#' @param sizes Logical: do you want to know the size occupied by the packages in your library?
+#'
 #' @return A data.frame containing the count of packages in each of the user's
 #'   libraries
 #' @export
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+lib_summary <- function(sizes = FALSE) {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
+
+  if (sizes) {
+    pkg_df$lib_size <- vapply(
+      pkg_df$Library,
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      },
+      FUN.VALUE = numeric(1)
+    )
+  }
   pkg_df
 }
--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -8,5 +8,11 @@ test_that("lib_summary returns expected results", {
 })
 
 test_that("lib_summary fails appropriately", {
-  expect_error(lib_summary("foo"), "unused argument")
+  expect_error(lib_summary(sizes = "foo"), "not interpretable as logical")
+})
+
+test_that("sizes argument works", {
+  res <- lib_summary(sizes = TRUE)
+  expect_equal(names(res), c("Library", "n_packages", "lib_size"))
+  expect_type(res$lib_size, "double")
 })
