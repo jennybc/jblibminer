2df713c3ba2e6ad6d738ac51a293fb5b979cf172
Jenny Bryan <jenny.f.bryan@gmail.com>
2024-06-14 10:07:59 +0200

    Create lib_summary()

---
+++ R/lib-summary.R
@@ -0,0 +1,7 @@
+lib_summary <- function() {
+  pkgs <- utils::installed.packages()
+  pkg_tbl <- table(pkgs[, "LibPath"])
+  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
+  names(pkg_df) <- c("Library", "n_packages")
+  pkg_df
+}
