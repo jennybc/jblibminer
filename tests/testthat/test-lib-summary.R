a91e04a7fd2fd9760c7e3b3de5d4f339f819497d
Jenny Bryan <jenny.f.bryan@gmail.com>
2024-06-14 11:40:44 +0200

    Starting testing the package

---
+++ tests/testthat/test-lib-summary.R
@@ -0,0 +1,12 @@
+test_that("lib_summary returns expected results", {
+  res <- lib_summary()
+  expect_s3_class(res, "data.frame")
+  expect_equal(ncol(res), 2)
+  expect_equal(names(res), c("Library", "n_packages"))
+  expect_type(res$Library, "character")
+  expect_type(res$n_packages, "integer")
+})
+
+test_that("lib_summary fails appropriately", {
+  expect_error(lib_summary("foo"), "unused argument")
+})
