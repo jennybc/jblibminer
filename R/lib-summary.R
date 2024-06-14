59c11d07ecc63a3baca0bb8e999bc1e01efe82b8
Jenny Bryan <jenny.f.bryan@gmail.com>
2024-06-14 11:15:32 +0200

    Add documentation

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,3 +1,14 @@
+#' R Library Summary
+#'
+#' Provides a brief summary of the package libraries on your machine. This is
+#' Jenny!
+#'
+#' @return A data.frame containing the count of packages in each of the user's
+#'   libraries
+#' @export
+#'
+#' @examples
+#' lib_summary()
 lib_summary <- function() {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
